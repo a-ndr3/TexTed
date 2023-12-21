78fbf61a0d31854e845ce3f28593019ff0a823fe
Andrei Blokhin <andrei_blokhin@outlook.com>
2023-12-21 16:24:39 +0100

    Add project files.

---
+++ TexTed/PieceList.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows;
+
+namespace TexTed
+{
+    internal class PieceList
+    {
+        private Piece head;
+        private string filePath;
+        private string scratchFilePath; //to handle insertion
+
+        public PieceList(string filePath)
+        {
+            this.filePath = filePath;
+
+            if (File.Exists(filePath + ".scratch"))
+            {
+                File.Delete(filePath + ".scratch");
+            }
+
+            this.scratchFilePath = filePath + ".scratch";
+
+            head = null;
+            LoadFromFile();
+        }
+
+        private void LoadFromFile()
+        {
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath);
+                return;
+            }
+
+            using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                StreamReader reader = new StreamReader(fs);
+                long currentPos = 0;
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    byte[] buffer = Encoding.UTF8.GetBytes(line + Environment.NewLine);
+                    Piece piece = new Piece(buffer.Length, filePath, currentPos, "DefaultFont", "DefaultStyle");
+                    currentPos += buffer.Length;
+
+                    AddPieceToEnd(piece);
+                }
+            }
+        }
+
+        private void AddPieceToEnd(Piece piece)
+        {
+            if (head == null)
+            {
+                head = piece;
+            }
+            else
+            {
+                Piece current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = piece;
+            }
+        }
+        private void WritePieceToFile(string text, Piece piece)
+        {
+            using (var fs = new FileStream(filePath, FileMode.Append, FileAccess.Write))
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(text);
+                fs.Write(buffer, 0, buffer.Length);
+                piece.FilePos = fs.Length - buffer.Length;
+                piece.Length = buffer.Length;
+            }
+        }
+       
+        //todo: check if needed, change 0s pos
+        public void AddPiece(string text, string font, string style)
+        {
+            Piece newPiece = new Piece(0, filePath, 0, font, style);
+           
+            WritePieceToFile(text, newPiece);
+
+            if (head == null)
+            {
+                head = newPiece;
+            }
+            else
+            {
+                Piece current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newPiece;
+            }
+        }
+       
+        public string GetAllText()
+        {
+            StringBuilder sb = new StringBuilder();
+            Piece current = head;
+            while (current != null)
+            {
+                sb.Append(current.GetText());
+                current = current.Next;
+            }
+            return sb.ToString();
+        }
+
+        //Split from slides
+        public Piece Split(int pos)
+        {
+            Piece p = head;
+            int accumulatedLength = p.Length;
+
+            while (pos > accumulatedLength && p.Next != null)
+            {
+                p = p.Next;
+                accumulatedLength += p.Length;
+            }
+
+            if (pos != accumulatedLength)
+            {
+                int len2 = accumulatedLength - pos;
+                int len1 = p.Length - len2;
+                p.Length = len1;
+
+                Piece q = new Piece(len2, p.File, p.FilePos + len1, p.Font, p.Style)
+                {
+                    Next = p.Next
+                };
+                p.Next = q;
+            }
+
+            return p;
+        }
+
+        //todo: should we create a new piece for EACH char? What to do with inserted string instead of char? Convert bunch of chars to string aka to one piece
+        public void Insert(int pos, char ch)
+        {
+            Piece p = Split(pos);
+
+            // write char to the scratch file
+            using (var fs = new FileStream(scratchFilePath, FileMode.Append, FileAccess.Write))
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(new char[] { ch });
+                fs.Write(buffer, 0, buffer.Length);
+            }
+
+            // create new piece for the char
+            Piece newPiece = new Piece(1, scratchFilePath, GetFileLength(scratchFilePath) - 1, "DefaultFont", "DefaultStyle");
+
+            // adjust linked list
+            if (p != null)
+            {
+                newPiece.Next = p.Next;
+                p.Next = newPiece;
+            }
+            else // if insert beginning
+            {
+                newPiece.Next = head;
+                head = newPiece;
+            }
+        }
+
+        private long GetFileLength(string filePath)
+        {
+            using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                return fs.Length;
+            }
+        }
+
+        //Delete from slides
+        public void Delete(int from, int to)
+        {
+            Piece a = Split(from);
+            Piece b = Split(to);
+            a.Next = b.Next;
+        }
+    }
+}
---
+++ TexTed/TextViewer.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using System.Windows.Media.TextFormatting;
+
+namespace TexTed
+{
+    internal class TextViewer : Control
+    {
+        private PieceList pieceList;
+        private int caretPosition;
+        private bool showCaret;
+        public string FilePath
+        {
+            set
+            {
+                pieceList = new PieceList(value);
+            }
+        }
+        public TextViewer()
+        {
+            this.caretPosition = 0;
+            this.showCaret = true;
+            this.Focusable = true;
+
+            this.Loaded += (sender, args) => Keyboard.Focus(this);
+
+            InitializeCaretBlinking();
+        }
+
+        private void InitializeCaretBlinking()
+        {
+            DispatcherTimer caretTimer = new DispatcherTimer();
+
+            caretTimer.Interval = TimeSpan.FromMilliseconds(500);
+
+            caretTimer.Tick += (sender, e) =>
+            {
+                showCaret = !showCaret;
+                InvalidateVisual(); // redraw
+            };
+            caretTimer.Start();
+        }
+
+        //todo: check render details because now I pass font/size
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            Typeface typeface = new Typeface("Arial");
+
+            double fontSize = 14;
+
+            Point start = new Point(0, fontSize);
+
+            string text = pieceList.GetAllText();
+
+            DrawText(drawingContext, text, start, fontSize, typeface);
+
+            if (showCaret)
+            {
+                DrawCaret(drawingContext, text, caretPosition, start, fontSize, typeface);
+            }
+        }
+
+        protected override void OnTextInput(TextCompositionEventArgs e)
+        {
+            base.OnTextInput(e);
+
+            foreach (char c in e.Text)
+            {
+                if (!char.IsControl(c))
+                {
+                    pieceList.Insert(caretPosition, c);
+                    caretPosition++;
+                }
+            }
+
+            InvalidateVisual();
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            string text = pieceList.GetAllText();
+            int textLength = text.IndexOf('\0');
+            if (textLength == -1) textLength = text.Length;
+
+            switch (e.Key)
+            {
+                case Key.Left:
+                    if (caretPosition > 0)
+                    {
+                        caretPosition--;
+                    }
+                    break;
+                case Key.Right:
+                    if (caretPosition < textLength)
+                    {
+                        caretPosition++;
+                    }
+                    break;
+                case Key.Back:
+                    if (caretPosition > 0)
+                    {
+                        pieceList.Delete(caretPosition - 1, caretPosition);
+                        caretPosition--;
+                    }
+                    break;
+            }
+
+            InvalidateVisual();
+
+        }
+
+
+        private void DrawText(DrawingContext drawingContext, string text, Point start, double fontSize, Typeface typeface)
+        {
+            if (string.IsNullOrEmpty(text)) return;
+
+            GlyphTypeface glyphTypeface;
+            
+            if (!typeface.TryGetGlyphTypeface(out glyphTypeface))
+            {
+                throw new InvalidOperationException("No glyph typeface found");
+            }
+
+            List<ushort> glyphIndexes = new List<ushort>();
+            List<double> advanceWidths = new List<double>();
+
+            foreach (char c in text)
+            {
+                if (char.IsControl(c)) continue;
+
+                if (glyphTypeface.CharacterToGlyphMap.TryGetValue(c, out ushort glyphIndex))
+                {
+                    glyphIndexes.Add(glyphIndex);
+                    advanceWidths.Add(glyphTypeface.AdvanceWidths[glyphIndex] * fontSize);
+                }
+
+            }
+
+            GlyphRun glyphRun = new GlyphRun(glyphTypeface, 0, false, fontSize, glyphIndexes.ToArray(), start, advanceWidths.ToArray(), null, null, null, null, null, null);
+            drawingContext.DrawGlyphRun(Brushes.Black, glyphRun);
+        }
+
+        private void DrawCaret(DrawingContext drawingContext, string text, int caretPosition, Point start, double fontSize, Typeface typeface)
+        {
+            double caretX = GetCaretXPosition(text, caretPosition, fontSize, typeface);
+
+            Point caretStart = new Point(start.X + caretX, start.Y - fontSize);
+            Point caretEnd = new Point(start.X + caretX, start.Y);
+
+            drawingContext.DrawLine(new Pen(Brushes.Black, 1), caretStart, caretEnd);
+        }
+
+        private double GetCaretXPosition(string text, int caretPosition, double fontSize, Typeface typeface)
+        {
+            string substring = text.Substring(0, caretPosition);
+            return MeasureTextWidth(substring, fontSize, typeface);
+        }
+
+        private double MeasureTextWidth(string text, double fontSize, Typeface typeface)
+        {
+            GlyphTypeface glyphTypeface;
+            if (!typeface.TryGetGlyphTypeface(out glyphTypeface))
+            {
+                throw new InvalidOperationException("No glyph typeface found");
+            }
+
+            double width = 0;
+            foreach (char c in text)
+            {
+                if (char.IsControl(c)) continue;
+
+                if (glyphTypeface.CharacterToGlyphMap.TryGetValue(c, out ushort glyphIndex))
+                {
+                    width += glyphTypeface.AdvanceWidths[glyphIndex] * fontSize;
+                }
+
+            }
+            return width;
+        }
+
+    }
+}
+
+// MUST HAVE TODO LIST
+//todo: add selection
+//todo: add styles/fonts  //handle styles/fonts for EACH piece, add handling at the beginning of each piece already added fields in Piece class
+//todo: add text formatting
+//todo: add undo/redo
+//todo: add save/load
+//todo: add scroll
+//todo: long lines are not wrapped but simply clipped at the right margin
+//todo: select words with double click
+
+
+// OTHER
+//todo: other stuff from the file
